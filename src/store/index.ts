3fcbfdd9499eaedaea06f15ee65680efc43b742e
rick <rickxtha@gmail.com>
2025-06-26 16:34:35 +0545

    yetikai post vayexa guys..

--- src/store/index.ts
+++ src/store/index.ts
@@ -19,15 +19,7 @@ let store = configureStore({
   reducer,
 });
 
-/*************  ✨ Windsurf Command ⭐  *************/
-  /**
-   * Creates a store instance.
-   * If on the server, it will just create a new store with reducers.
-   * If on the client, it will use the existing store (if available) or create a new one.
-   * The store is then wrapped with persistStore to allow for local storage persistence.
-   * @returns {Store} The store instance.
-   */
-/*******  2b126cd8-0800-4409-a6d5-e18eac8086a4  *******/
+
 const makeStore = () => {
   // Detect if running on the server
   const isServer = typeof window === "undefined";
